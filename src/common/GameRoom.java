e985297f7edb4c6e3ce7c357c5ca62e8747f2c4d
JudeRester <36389982+JudeRester@users.noreply.github.com>
2019-11-25 03:47:10 +0900

    image files

--- src/common/GameRoom.java
+++ src/common/GameRoom.java
@@ -2,18 +2,27 @@ package common;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameRoom implements Serializable{
 	private int seq;
 	private String rName;
 	private ArrayList<User> users= new ArrayList<>();
 	private boolean isStart = false;
-	
+	private Map<String, int[]> m = Collections.synchronizedMap(new HashMap<String, int[]>());
+
+
 	
 	public GameRoom(User user, int seq, String rName) {
 		users.add(user);
 		this.seq=seq;
 		this.rName=rName;
+	}
+	public void gameStart() {
+		m.put(users.get(1).getId(), new int[13]);
+		m.put(users.get(2).getId(), new int[13]);
 	}
 	public int getSeq() {
 		return seq;
